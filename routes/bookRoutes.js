830b9ae347da7039f38d14690a3c67bd91e06185
Sid <sidmirza4@gmail.com>
2021-03-03 11:34:56 +0530

    edit book access rules

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,14 +1,19 @@
 const express = require('express');
 
 const bookController = require('../controllers/bookController');
+const { restrictTo } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.route('/').get(bookController.getAllBooks).post(bookController.createBook);
+router
+	.route('/')
+	.get(bookController.getAllBooks)
+	.post(restrictTo('editor'), bookController.createBook);
+
 router
 	.route('/:id')
 	.get(bookController.getBook)
-	.patch(bookController.updateBook)
-	.delete(bookController.deleteBook);
+	.patch(restrictTo('editor'), bookController.updateBook)
+	.delete(restrictTo('admin'), bookController.deleteBook);
 
 module.exports = router;
